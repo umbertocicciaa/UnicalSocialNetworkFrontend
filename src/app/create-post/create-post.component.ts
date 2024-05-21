ceaee3574a810acf049c21386ea0ec14baf726db
umbertodomenicociccia <umbertociccia@icloud.com>
2024-05-21 21:53:21 +0200

    fix import

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 @Component({
   selector: "app-create-post",
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, CommonModule],
+  imports: [FormsModule, MatFormFieldModule, CommonModule, MatInputModule],
   templateUrl: "./create-post.component.html",
   styleUrl: "./create-post.component.css",
 })
--- src/app/create-twit/create-twit.component.ts
+++ src/app/create-twit/create-twit.component.ts
@@ -2,11 +2,11 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
-
+import { MatInputModule } from "@angular/material/input";
 @Component({
   selector: "app-create-twit",
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, CommonModule],
+  imports: [FormsModule, MatFormFieldModule, CommonModule, MatInputModule],
   templateUrl: "./create-twit.component.html",
   styleUrl: "./create-twit.component.css",
 })
