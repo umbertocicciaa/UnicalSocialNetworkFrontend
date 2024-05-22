43b1756ac85f8eb900cbb30ec86e26f3b5df27f3
umbertodomenicociccia <umbertociccia@icloud.com>
2024-05-22 18:26:03 +0200

    migliorata home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { AuthService, UserService } from "../api/services";
+import { firstValueFrom } from "rxjs";
+
 @Component({
   selector: "app-home",
   standalone: true,
@@ -8,4 +11,26 @@ import { MatSidenavModule } from "@angular/material/sidenav";
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private currentUserName: string = "";
+
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+    try {
+      let user = await firstValueFrom(this.userService.getLoggedUsers());
+      this.currentUserName = user.profileName ?? "";
+    } catch (err) {}
+  }
+
+  navigateToUsername() {
+    if (this.currentUserName)
+      this.router.navigate(["/home/profile/" + this.currentUserName]);
+  }
+
+  logout() {}
+}
