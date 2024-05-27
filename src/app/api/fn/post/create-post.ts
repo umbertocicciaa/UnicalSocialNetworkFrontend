f206379629080b24b57512c8b86d27ffd6b5978a
umbertodomenicociccia <umbertociccia@icloud.com>
2024-05-27 17:14:47 +0200

    create post

--- src/app/api/fn/post/create-post.ts
+++ src/app/api/fn/post/create-post.ts
@@ -1,32 +1,43 @@
 /* tslint:disable */
 /* eslint-disable */
-import { HttpClient, HttpContext, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { StrictHttpResponse } from '../../strict-http-response';
-import { RequestBuilder } from '../../request-builder';
+import { HttpClient, HttpContext, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { filter, map } from "rxjs/operators";
+import { StrictHttpResponse } from "../../strict-http-response";
+import { RequestBuilder } from "../../request-builder";
 
-import { PostCreatedResponse } from '../../models/post-created-response';
-import { PostCreateRequest } from '../../models/post-create-request';
+import { PostCreatedResponse } from "../../models/post-created-response";
+import { PostCreateRequest } from "../../models/post-create-request";
 
 export interface CreatePost$Params {
-      body: PostCreateRequest
+  body: PostCreateRequest;
 }
 
-export function createPost(http: HttpClient, rootUrl: string, params: CreatePost$Params, context?: HttpContext): Observable<StrictHttpResponse<PostCreatedResponse>> {
-  const rb = new RequestBuilder(rootUrl, createPost.PATH, 'post');
+export function createPost(
+  http: HttpClient,
+  rootUrl: string,
+  params: CreatePost$Params,
+  context?: HttpContext
+): Observable<StrictHttpResponse<PostCreatedResponse>> {
+  const rb = new RequestBuilder(rootUrl, createPost.PATH, "post");
   if (params) {
-    rb.body(params.body, 'application/json');
+    rb.body(params.body, "application/json");
   }
 
-  return http.request(
-    rb.build({ responseType: 'json', accept: 'application/hal+json', context })
-  ).pipe(
-    filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
-    map((r: HttpResponse<any>) => {
-      return r as StrictHttpResponse<PostCreatedResponse>;
-    })
-  );
+  return http
+    .request(
+      rb.build({
+        responseType: "json",
+        accept: "application/hal+json",
+        context,
+      })
+    )
+    .pipe(
+      filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return r as StrictHttpResponse<PostCreatedResponse>;
+      })
+    );
 }
 
-createPost.PATH = '/api/v1/Post/posts/posts';
+createPost.PATH = "/api/v1/Post/posts/posts";
--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -3,18 +3,32 @@ import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { PostService } from "../api/services";
+import { SpinnerLoadComponent } from "../spinner-load/spinner-load.component";
+import { firstValueFrom } from "rxjs";
+import { ErrorComponent } from "../error/error.component";
 @Component({
   selector: "app-create-post",
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, CommonModule, MatInputModule],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    CommonModule,
+    MatInputModule,
+    SpinnerLoadComponent,
+    ErrorComponent,
+  ],
   templateUrl: "./create-post.component.html",
   styleUrl: "./create-post.component.css",
 })
 export class CreatePostComponent {
   imagePreview: string | ArrayBuffer | null = null;
   caption: string = "";
-
-  constructor() {}
+  selectedFile!: File;
+  loading: boolean = false;
+  error: boolean = false;
+  completato: boolean = false;
+  constructor(private postService: PostService) {}
 
   onFileSelected(event: Event): void {
     const file = (event.target as HTMLInputElement).files![0];
@@ -23,14 +37,38 @@ export class CreatePostComponent {
     reader.onload = () => {
       this.imagePreview = reader.result;
     };
-
+    this.selectedFile = file;
     reader.readAsDataURL(file);
   }
 
-  onSubmit(): void {
-    // Logic to handle the post creation
-    console.log("Post submitted");
-    console.log("Caption:", this.caption);
-    console.log("Image:", this.imagePreview);
+  async onSubmit() {
+    if (!this.selectedFile) return;
+    this.loading = true;
+    const base64String = await this.convertFileToBase64(this.selectedFile);
+    await firstValueFrom(
+      this.postService.createPost({
+        body: {
+          caption: this.caption,
+          mediaFile: base64String,
+        },
+      })
+    )
+      .then(() => (this.error = false))
+      .catch((err) => {
+        this.error = true;
+      })
+      .finally(() => (this.loading = false));
+  }
+
+  private convertFileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const base64String = (reader.result as string).split(",")[1];
+        resolve(base64String);
+      };
+      reader.onerror = (error) => reject(error);
+    });
   }
 }
