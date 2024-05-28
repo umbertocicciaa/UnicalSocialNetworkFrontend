675687f2f8b8a9899918c3341b11987be13246a1
umbertodomenicociccia <umbertociccia@icloud.com>
2024-05-28 12:41:32 +0200

    logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { UserService } from "../api/services";
 import { firstValueFrom } from "rxjs";
 import { UserResponse } from "../api/models";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-home",
@@ -14,8 +15,12 @@ import { UserResponse } from "../api/models";
 })
 export class HomeComponent implements OnInit {
   private currentUser: UserResponse = {};
-
-  constructor(private userService: UserService, private router: Router) {}
+  private api = "http://localhost:8080/api/v1/auth/logout";
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   async ngOnInit() {
     await firstValueFrom(this.userService.getLoggedUsers()).then(
@@ -36,5 +41,10 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  logout() {}
+  async logout() {
+    await firstValueFrom(this.http.post(this.api, undefined)).then(() => {
+      localStorage.removeItem("token");
+      this.router.navigate(["/login"]);
+    });
+  }
 }
