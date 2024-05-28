890c62101f1b3df613e2fb46cf4853f6267ac25a
umbertodomenicociccia <umbertociccia@icloud.com>
2024-05-28 12:41:53 +0200

    import fix

--- src/app/twit/twit.component.ts
+++ src/app/twit/twit.component.ts
@@ -5,7 +5,7 @@ import { PostService } from "../api/services";
 import { firstValueFrom } from "rxjs";
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
-import { Route, Router } from "@angular/router";
+import { Router } from "@angular/router";
 
 const THUMBUP_ICON =
   `
